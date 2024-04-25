32b5563a66f4de47e48108e6d982aac874455465
Erizium <amin.69540@gmail.com>
2024-04-25 02:31:50 +0200

    skeleton for dataset loading functions

---
+++ code/load_datasets.py
@@ -0,0 +1,49 @@
+import numpy as np
+import random
+
+datasetPath = "../datasets"
+
+
+def load_iris_np():
+    """Load the Iris dataset into a numpy array."""
+    pass
+
+
+def load_iris_dataset(train_ratio):
+    """Load the Iris dataset and split it into train and test sets."""
+    pass
+
+
+def load_iris_dataset_folds(train_ratio, n_folds):
+    """Load the Iris dataset and split it into train and test sets for cross-validation."""
+    pass
+
+
+def load_wine_np():
+    """Load the Binary Wine quality dataset into a numpy array."""
+    pass
+
+
+def load_wine_dataset(train_ratio):
+    """Load the Binary Wine quality dataset and split it into train and test sets."""
+    pass
+
+
+def load_wine_dataset_folds(train_ratio, n_folds):
+    """Load the Binary Wine quality dataset and split it into train and test sets for cross-validation."""
+    pass
+
+
+def load_abalone_np():
+    """Load the Abalone-intervalles dataset into a numpy array."""
+    pass
+
+
+def load_abalone_dataset(train_ratio):
+    """Load the Abalone-intervalles dataset and split it into train and test sets."""
+    pass
+
+
+def load_abalone_dataset_folds(train_ratio, n_folds):
+    """Load the Abalone-intervalles dataset and split it into train and test sets for cross-validation."""
+    pass
